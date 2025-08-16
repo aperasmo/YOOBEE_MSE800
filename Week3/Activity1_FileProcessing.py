aeda5ca098bc900948d4d3878d636a06b699f665
aerasmo <aerasmo@iripple.net>
2025-08-16 18:45:04 +1200

    Activity1
    Added some comments

--- Week3/Activity1_FileProcessing.py
+++ Week3/Activity1_FileProcessing.py
@@ -2,21 +2,25 @@
 #Share the GitHub link here(with adding the screenshot of the result).
 
 
-class ProcessTextFile:
-    
-    def file_reader(self,file_path):
-     with open(file_path, "r") as file:
+class ProcessTextFile: # Class to handle file processing operations
+    def __init__(self, file_path): # Initialize with the file path
+        self.file_path = file_path
+
+    def file_reader(self,file_path): # Read the contents of the file
+     with open(self.file_path, 'r', encoding='utf-8') as file: # Open the file in read mode with UTF-8 encoding
       content = file.read()
       print(f"contents of the file is {content} ")
   
-    def write_into_file(self, additional_file_content, file_path):
+    def write_into_file(self, additional_content, file_path): # Write additional content to the file
         with open(file_path,"a") as file:
-         file.write(additional_file_content)
-
+         file.write(additional_content)
+    
 
 if __name__ == "__main__":
- file_path = "demo.txt"
- file_read_writer = ProcessTextFile()
- file_read_writer.file_reader(file_path)
- file_read_writer.write_into_file("I over write the content of this file",file_path)
- file_read_writer.file_reader(file_path)
+ file_path = "demo.txt" #   Specify the path to the demo.txt file
+ file_manipulator = ProcessTextFile(file_path) # Create an instance of ProcessTextFile
+ file_manipulator.file_reader(file_path) # Read the file contents
+ file_manipulator.write_into_file("**************************************\nI APPEND SOME TEXT HERE IN DEMO FILE",file_path) # Write additional content to the file
+ file_manipulator.file_reader(file_path) # Read the file contents again to show the updated content
+
+ 
